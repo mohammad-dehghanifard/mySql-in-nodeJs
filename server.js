2971391143722ce5abac91af44556a886e3a1fd2
mohammad <m.deheghanifard@gmail.com>
2024-04-27 09:18:47 +0330

    Basic web service implementation

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
-const dotenv = require('dotenv').config();
-const db = require('./service/db_config');
+const express = require('express')
+require("dotenv").config()
+
+const app = express()
+
+app.use(express.json())
+
+app.use("/posts",(req,res) => {
+    return next("no impl...")
+})
+
+app.use((req,res,err,next) =>{
+    console.log(err)
+
+    res.status(500).json(
+        {
+            "message" : "Operation Field..."
+        }
+    )
+})
+const  port = process.env.Port || 3000
+app.listen(port,() => console.log(`node js app run in ${port}`))
